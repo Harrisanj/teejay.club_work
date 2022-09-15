15416a0242f62b7b317a82beb5e6ba2a8eade7fe
TeeJayAdmin <tarp.boogies.0l@icloud.com>
2022-09-16 01:19:38 +0400

    Add new donate page

--- apps/web/src/pages/donate.tsx
+++ apps/web/src/pages/donate.tsx
@@ -1,7 +1,18 @@
+import { HeartIcon } from "@heroicons/react/20/solid";
+import { CheckIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
+import { Fragment, memo, useState } from "react";
+
 import { Card } from "../components/card";
 import { NewComments } from "../components/comments";
+import { Link } from "../components/link";
 import { Page } from "../components/page";
 import { withInitialData } from "../utilities";
+
+const template = encodeURIComponent(`Добрый день!
+                  
+Я пожертвовал клубу некоторую сумму денег в валюте {BTC,LTC,ETH}.
+Мой {логин,имя} на сайте: username.
+Номер транзакции: f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16.`);
 
 export const getServerSideProps = withInitialData(async () => {
   return { props: {} };
@@ -15,20 +26,66 @@ export default function Donate() {
           <h1 className="!text-2xl">Поддержать клуб TeeJay</h1>
           <div className="mt-2 flex flex-col gap-y-3 content">
             <p>
-              Клуб TeeJay развивается только за счёт сообщества. Вы тоже можете
-              поучаствовать в его развитии. Для этого не обязательно уметь
-              писать код. Можно помочь тестированием, финансами, железом,
-              консультацией, идеями, модераторством, контентом и т.д. и т.п. Для
-              этого вы можете перейти в Discord по ссылке в левом меню.
+              Клуб TeeJay развивается только за счёт сообщества и вы тоже можете
+              поучаствовать в его развитии.
+            </p>
+            <p>
+              Самое главное на сайте — это контент. Поэтому, если вы напишите
+              статью или комментарий, то вы уже сделаете вклад в развитие клуба.
+            </p>
+            <p>
+              Найдя ошибку на сайте, вы можете напишите в подсайт{" "}
+              <Link href="/subsites/bugs">Сломалось</Link>.
+            </p>
+            <p>
+              Веб-разработчики, мобильные разработчики, тестировщики и дизайнеры
+              могут присоединиться к разработке. Заходите в{" "}
+              <a
+                href="https://discord.gg/qr2uyNh2fE"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Discord
+              </a>{" "}
+              и{" "}
+              <a
+                href="https://github.com/teejayclub/teejay.club"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+              .
             </p>
             <p>
               Есть возможность помочь финансами? Вы можете сделать пожертвование
-              в криптовалюте.
+              в криптовалюте по реквизитам, которые указаны ниже.
             </p>
-            <h2>BTC</h2>
-            <code className="-mx-4 p-4 overflow-auto">
-              bc1qpz5zan2re29jufv85l58ha2ucvkqxuuq7k7kla
-            </code>
+            <p>
+              Все, кто сделает пожертвование, получат уникальный значок в виде
+              сердца <HeartIcon className="inline w-5 h-5 fill-red-500" /> рядом
+              с ником. Для этого напишите на почту{" "}
+              <a
+                href={`mailto:admin@teejay.club?subject=Пожертвование&body=${template}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                admin@teejay.club
+              </a>{" "}
+              номер транзакции и логин/имя в клубе.
+            </p>
+            <CryptoWallet
+              name="BTC"
+              address="bc1qpz5zan2re29jufv85l58ha2ucvkqxuuq7k7kla"
+            />
+            <CryptoWallet
+              name="LTC"
+              address="ltc1qvuxnvp7xl2jh67uk5lpe2250s5e747ls5v7u24"
+            />
+            <CryptoWallet
+              name="ETH"
+              address="0x750F7321a8278f3d2e9D1b6Fd0b38d1b597406dF"
+            />
           </div>
         </Card>
       </div>
@@ -36,3 +93,33 @@ export default function Donate() {
     </Page>
   );
 }
+
+type CryptoWalletProps = {
+  name: string;
+  address: string;
+};
+
+const CryptoWallet = memo<CryptoWalletProps>(({ name, address }) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const handleClick = () => {
+    navigator.clipboard.writeText(address).then(() => setIsCopied(true));
+  };
+  return (
+    <Fragment>
+      <h2>{name}</h2>
+      <code className="-mx-4 p-4 flex flex-row justify-between items-center overflow-auto">
+        <span>{address}</span>
+        {isCopied ? (
+          <CheckIcon className="w-6 h-6  stroke-green-500" />
+        ) : (
+          <DocumentDuplicateIcon
+            className="w-6 h-6 cursor-pointer"
+            onClick={handleClick}
+          />
+        )}
+      </code>
+    </Fragment>
+  );
+});
+
+CryptoWallet.displayName = "CryptoWallet";
