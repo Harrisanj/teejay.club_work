aa89dfb9f718034b141856dccd4145658e7e5439
TeeJayAdmin <tarp.boogies.0l@icloud.com>
2022-10-12 22:56:19 +0400

    Add image caching

--- apps/api/src/avatars.ts
+++ apps/api/src/avatars.ts
@@ -38,7 +38,10 @@ export function avatars(
         })
         .promise();
 
-      return reply.header("Content-Type", "image/webp").send(output.Body);
+      return reply
+        .header("Content-Type", "image/webp")
+        .header("Cache-Control", "max-age=31536000")
+        .send(output.Body);
     } catch (error) {
       console.error(error);
       return reply.status(404).send();
